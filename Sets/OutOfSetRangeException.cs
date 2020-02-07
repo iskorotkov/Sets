671ebf8492dbae21e99dfcb526dd08bdffd8c5ef
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-07 20:46:57 +0500

    Change OutOfSetRangeException's base class to ApplicationException

--- Sets/OutOfSetRangeException.cs
+++ Sets/OutOfSetRangeException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Sets
 {
-    public class OutOfSetRangeException : Exception
+    public class OutOfSetRangeException : ApplicationException
     {
         public OutOfSetRangeException(int invalidValue, int max) : base($"Can't add value to set because it's not in supported range. Invalid value is {invalidValue}, and set range is [1, {max}].")
         { }
