3dd8e52e3b74bb44b556c189bb3946ad8205de99
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-08 20:07:49 +0500

    Refactor populating comboboxes in Test1Page

--- Sets.UWP/Test1Page.xaml.cs
+++ Sets.UWP/Test1Page.xaml.cs
@@ -17,43 +17,16 @@ namespace Sets.UWP
 
         private void FillSourceCb()
         {
-            var fromFile = new ComboBoxItem
-            {
-                Content = "From file",
-                Tag = new ReadingMethod {Execute = ReadFromFile, OnSelected = OnReadFromFileSelected}
-            };
-            var fromBox = new ComboBoxItem
-            {
-                Content = "From box",
-                Tag = new ReadingMethod {Execute = ReadFromBox, OnSelected = OnReadFromConsoleBox}
-            };
-
-            SourceCb.Items.Add(fromFile);
-            SourceCb.Items.Add(fromBox);
+            SourceCb.Items.Add(new ReadingMethod("From file", ReadFromFile, OnReadFromFileSelected));
+            SourceCb.Items.Add(new ReadingMethod("From box", ReadFromBox, OnReadFromConsoleBox));
             SourceCb.SelectedIndex = 0;
         }
 
         private void FillRepresentationCb()
         {
-            var logical = new ComboBoxItem
-            {
-                Content = "Logical",
-                Tag = new CreationMethod {Execute = CreateLogicalSet}
-            };
-            var bit = new ComboBoxItem
-            {
-                Content = "Bit",
-                Tag = new CreationMethod {Execute = CreateBitSet}
-            };
-            var multiset = new ComboBoxItem
-            {
-                Content = "Multiset",
-                Tag = new CreationMethod {Execute = CreateMultiSet}
-            };
-
-            RepresentationCb.Items.Add(logical);
-            RepresentationCb.Items.Add(bit);
-            RepresentationCb.Items.Add(multiset);
+            RepresentationCb.Items.Add(new CreationMethod("Logical", CreateLogicalSet));
+            RepresentationCb.Items.Add(new CreationMethod("Bit", CreateBitSet));
+            RepresentationCb.Items.Add(new CreationMethod("Multiset", CreateMultiSet));
             RepresentationCb.SelectedIndex = 0;
         }
 
@@ -65,12 +38,9 @@ namespace Sets.UWP
 
         private void createSetBtn_Click(object sender, RoutedEventArgs e)
         {
-            var representationItem = (ComboBoxItem) RepresentationCb.SelectedItem;
-            var sourceItem = (ComboBoxItem) SourceCb.SelectedItem;
-
             var max = (int) MaxElemSlider.Value;
-            var setCreator = (CreationMethod) representationItem.Tag;
-            var method = (ReadingMethod) sourceItem.Tag;
+            var setCreator = (CreationMethod) RepresentationCb.SelectedItem;
+            var method = (ReadingMethod) SourceCb.SelectedItem;
 
             var set = setCreator.Execute(max);
             method.Execute(set);
@@ -78,11 +48,11 @@ namespace Sets.UWP
             //Frame.Navigate(typeof(Test1Actions), set);
         }
 
-        private SimpleSet CreateLogicalSet(int max) => new SimpleSet(max);
+        private static SimpleSet CreateLogicalSet(int max) => new SimpleSet(max);
 
-        private BitSet CreateBitSet(int max) => new BitSet(max);
+        private static BitSet CreateBitSet(int max) => new BitSet(max);
 
-        private MultiSet CreateMultiSet(int max) => new MultiSet(max);
+        private static MultiSet CreateMultiSet(int max) => new MultiSet(max);
 
         private void OnReadFromFileSelected()
         {
@@ -96,30 +66,48 @@ namespace Sets.UWP
             SetContentTb.Visibility = Visibility.Visible;
         }
 
-        private void ReadFromBox(Set set)
+        private static void ReadFromBox(Set set)
         {
         }
 
-        private void ReadFromFile(Set set)
+        private static void ReadFromFile(Set set)
         {
         }
 
         private void SourceCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var item = (ComboBoxItem) SourceCb.SelectedItem;
-            var method = (ReadingMethod) item.Tag;
+            var method = (ReadingMethod) SourceCb.SelectedItem;
             method.OnSelected();
         }
 
-        private struct ReadingMethod
+        private class ReadingMethod
         {
-            public Action<Set> Execute;
-            public Action OnSelected;
+            public ReadingMethod(string text, Action<Set> execute, Action onSelected)
+            {
+                Text = text;
+                Execute = execute;
+                OnSelected = onSelected;
+            }
+
+            public string Text { get; }
+            public Action<Set> Execute { get; }
+            public Action OnSelected { get; }
+
+            public override string ToString() => Text;
         }
 
-        private struct CreationMethod
+        private class CreationMethod
         {
-            public Func<int, Set> Execute;
+            public CreationMethod(string text, Func<int, Set> execute)
+            {
+                Text = text;
+                Execute = execute;
+            }
+
+            public string Text { get; }
+            public Func<int, Set> Execute { get; }
+
+            public override string ToString() => Text;
         }
     }
 }
