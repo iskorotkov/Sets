796bb0ae5f856cb1f279f3ee746753bf077ac42a
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-08 19:46:43 +0500

    Add Sets.Lib project because Sets didn't work with UWP

---
+++ Sets.Lib/Set.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Sets
+{
+    public abstract class Set
+    {
+        public static Set operator +(Set lhs, Set rhs)
+        {
+            var length = Math.Max(lhs.Max, rhs.Max);
+            var result = (Set) Activator.CreateInstance(lhs.GetType(), length);
+            for (var i = 1; i <= length; i++)
+            {
+                if (lhs.Contains(i) || rhs.Contains(i))
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+
+        public static Set operator *(Set lhs, Set rhs)
+        {
+            var length = Math.Min(lhs.Max, rhs.Max);
+            var result = (Set) Activator.CreateInstance(lhs.GetType(), length);
+            for (var i = 1; i <= length; i++)
+            {
+                if (lhs.Contains(i) && rhs.Contains(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+
+        public abstract void Add(int i);
+        public abstract void Remove(int i);
+        public abstract bool Contains(int i);
+
+        protected readonly int Max;
+
+        protected Set(int max)
+        {
+            Max = max;
+        }
+
+        public void Append(string s)
+        {
+            var nums = s.Split(' ', '\t', '\r', '\n')
+                .Where(str => str.Any())
+                .Select(int.Parse)
+                .ToArray();
+            Append(nums);
+        }
+
+        public void Append(int[] nums)
+        {
+            Array.ForEach(nums, Add);
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.Append("Set: [");
+            var elemAdded = false;
+            for (var i = 1; i <= Max; ++i)
+            {
+                if (!Contains(i)) continue;
+                if (elemAdded)
+                {
+                    builder.Append(", ");
+                }
+
+                builder.Append(i);
+                elemAdded = true;
+            }
+
+            builder.Append("]");
+            return builder.ToString();
+        }
+    }
+}
---
+++ Sets.Lib/SimpleSet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Sets
+{
+    public class SimpleSet : Set
+    {
+        private readonly bool[] _arr;
+
+        public SimpleSet(int max) : base(max)
+        {
+            _arr = new bool[max];
+        }
+
+        public override void Add(int i)
+        {
+            if (i < 1 || i > Max)
+            {
+                throw new OutOfSetRangeException(i, Max);
+            }
+            _arr[i - 1] = true;
+        }
+
+        public override void Remove(int i)
+        {
+            if (i < 1 || i > Max) return;
+            _arr[i - 1] = false;
+        }
+
+        public override bool Contains(int i) => i >= 1 && i <= Max && _arr[i - 1];
+
+        public static SimpleSet operator +(SimpleSet lhs, SimpleSet rhs)
+        {
+            var minLength = Math.Min(lhs.Max, rhs.Max);
+            var length = Math.Max(lhs.Max, rhs.Max);
+            var result = new SimpleSet(length);
+            for (var i = 0; i < minLength; i++)
+            {
+                result._arr[i] = lhs._arr[i] || rhs._arr[i];
+            }
+
+            var biggest = lhs.Max > rhs.Max ? lhs : rhs;
+            for (var i = minLength; i < length; i++)
+            {
+                result._arr[i] = biggest._arr[i];
+            }
+
+            return result;
+        }
+
+        public static SimpleSet operator *(SimpleSet lhs, SimpleSet rhs)
+        {
+            var length = Math.Min(lhs.Max, rhs.Max);
+            var result = new SimpleSet(length);
+            for (var i = 0; i < length; i++)
+            {
+                result._arr[i] = lhs._arr[i] && rhs._arr[i];
+            }
+
+            return result;
+        }
+    }
+}
