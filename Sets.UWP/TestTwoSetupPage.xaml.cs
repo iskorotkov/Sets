fb0a9d7be55645dcfc97efc693fcc9fdc5ca8f6c
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-09 19:20:49 +0500

    Create layout for page with test 2

--- Sets.UWP/TestTwoSetupPage.xaml.cs
+++ Sets.UWP/TestTwoSetupPage.xaml.cs
@@ -13,5 +13,26 @@ namespace Sets.UWP
         {
             InitializeComponent();
         }
+
+        private void UpdateButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var max = (int) SetMaxSlider.Value;
+            ShowResultsForSets(new SimpleSet(max), new SimpleSet(max), LogicalUnion, LogicalIntersection);
+            ShowResultsForSets(new BitSet(max), new BitSet(max), BitUnion, BitIntersection);
+            ShowResultsForSets(new MultiSet(max), new MultiSet(max), MultisetUnion, MultisetIntersection);
+        }
+
+        private void ShowResultsForSets(Set set1, Set set2, TextBlock unionTb, TextBlock intersectionTb)
+        {
+            PopulateSets(set1, set2);
+            unionTb.Text = (set1 + set2).ToString();
+            intersectionTb.Text = (set1 * set2).ToString();
+        }
+
+        private void PopulateSets(Set set1, Set set2)
+        {
+            set1.Append(SetOneBox.Text);
+            set2.Append(SetTwoBox.Text);
+        }
     }
 }
